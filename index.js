dc42873b05c4574191530fd188f15f355b2f9a94
= <=>
2022-12-19 16:09:15 +0530

    add

---
+++ index.js
@@ -0,0 +1,61 @@
+// const child={
+//     name:"Ram",
+//     rollNo:10
+// }
+// const parents={
+//     city:"lko",
+//     mobileNo:12345,
+//     ___proto__:child
+// }
+// console.log(parents)
+
+
+// q2
+// function student(name,rollNo){
+//     this.name=name;
+//     this.rollNo=rollNo;
+// }
+// let student1= new student("rishabh",08)
+// let student2= new student("aryan",02)
+// console.log(student1);
+// console.log(student2);
+// student1.sports='cricket'
+
+// ques 3
+
+// arr1=[1,3,5,7,8]
+//         arr2=[8,5,6,4,6]
+//         arr3=[9,6,4,9,3]
+        
+//         const arraySum={
+//             add:function(arr){
+//                 let sum=0;
+//                 arr.forEach(element => {
+//                   sum+=element;
+//                 });
+//                 console.log(sum)
+//             }
+//         }
+        
+//         Array.__proto__=arraySum;
+//         Array.add(arr1);
+//         Array.add(arr2)
+//         Array.add(arr3)
+
+
+// question-4
+
+// function student(name,rollNo){
+//     this.name=name;
+//     this.rollNo=rollNo;
+// }
+// let student1= new student("rishabh",08)
+// let student2= new student("aryan",02)
+// console.log(student1);
+// console.log(student2);
+// student1.sports='cricket'
+
+// console.log(Object.getOwnPropertyNames(student1))
+
+
+
